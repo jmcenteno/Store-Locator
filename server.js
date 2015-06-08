e6e5fcc6dd6f4f5db3bb000940e6305c1aa28e43
jcenteno <jose@jmfield.com>
2015-06-08 09:57:57 -0400

    - show message on console after server has been started

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/')).listen(8000);
+var port = 8000;
+
+connect().use(serveStatic(__dirname + '/')).listen(port);
+console.log('Server running on http://localhost: ' + port);
